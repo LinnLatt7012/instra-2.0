739cb318d2e2fd61aff64c2318574965cb9356c8
plinnlatt <plinnlatt70@gmail.com>
2021-11-28 10:44:35 +0630

    Instra 2.0 tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}',
-  './components/**/*.{js,ts,jsx,tsx}',
+  './Components/**/*.{js,ts,jsx,tsx}',
   './layout/**/*.{js,ts,jsx,tsx}',
   './helpers/**/*.{js,ts,jsx,tsx}',,],
   darkMode: 'class', // or 'media' or 'class'
